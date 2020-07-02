1c852336648aaf6b5d591eff6281b949824920d2
Lukas Grünwald <gruenwald.lk@outlook.com>
2020-07-02 10:32:42 +0200

    Adds missing .ctor

--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/File/Type/Definition/MegFile.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/File/Type/Definition/MegFile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
@@ -11,6 +12,14 @@ namespace PG.StarWarsGame.Files.MEG.Binary.File.Type.Definition
         [NotNull] private readonly MegHeader m_header;
         [NotNull] private readonly MegFileNameTable m_fileNameTable;
         [NotNull] private readonly MegFileContentTable m_fileContentTable;
+
+        public MegFile(MegHeader header, MegFileNameTable fileNameTable, MegFileContentTable fileContentTable)
+        {
+            m_header = header ?? throw new ArgumentNullException(nameof(header));
+            m_fileNameTable = fileNameTable ?? throw new ArgumentNullException(nameof(fileNameTable));
+            m_fileContentTable = fileContentTable ?? throw new ArgumentNullException(nameof(fileContentTable));
+        }
+
         public byte[] ToBytes()
         {
             List<byte> b = new List<byte>();

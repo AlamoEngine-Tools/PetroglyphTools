3aae7c6d4068eca00d8b4afa833f9032a25139fe
Lukas Grünwald <gruenwald.lk@outlook.com>
2020-09-12 17:03:16 +0200

    Initializes *.Tools.Build Project
    - Added new project.
    - Updated dependencies.

---
+++ PG.StarWarsGame.Tools.Build/Host.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PG.StarWarsGame.Tools.Build
+{
+    internal class Host
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void ExecInternal()
+        {
+        }
+
+        private static void HandleParseErrorsInternal()
+        {
+        }
+    }
+}

1fd13a70cc17b105c1092c574d6fb73eb90b2a62
Lukas Grünwald <gruenwald.lk@outlook.com>
2021-02-06 16:43:56 +0100

    Initial Cleanup

--- PG.StarWarsGame.Localisation/PG.StarWarsGame.Localisation/Data/Config/v1/LocalisationType.cs
+++ PG.StarWarsGame.Localisation/PG.StarWarsGame.Localisation/Data/Config/v1/LocalisationType.cs
@@ -1,43 +1,60 @@
+// Copyright (c) 2021 Alamo Engine Tools and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
 namespace PG.StarWarsGame.Localisation.Data.Config.v1
 {
-    /// <remarks/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
-    [System.SerializableAttribute()]
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://www.example.org/eaw-translation/")]
-    public class LocalisationType : object, System.ComponentModel.INotifyPropertyChanged {
-    
-        private TranslationType[] m_translationDataField;
-    
+    /// <remarks />
+    [ExcludeFromCodeCoverage]
+    [GeneratedCode("xsd", "4.8.3928.0")]
+    [Serializable]
+    [DebuggerStepThrough]
+    [DesignerCategory("code")]
+    [XmlType(Namespace = "http://www.example.org/eaw-translation/")]
+    public class LocalisationType : object, INotifyPropertyChanged
+    {
         private string m_keyField;
-    
-        /// <remarks/>
-        [System.Xml.Serialization.XmlArrayAttribute(Form=System.Xml.Schema.XmlSchemaForm.Unqualified)]
-        [System.Xml.Serialization.XmlArrayItemAttribute("Translation", Form=System.Xml.Schema.XmlSchemaForm.Unqualified, IsNullable=false)]
-        public TranslationType[] TranslationData {
+
+        private TranslationType[] m_translationDataField;
+
+        /// <remarks />
+        [XmlArray(Form = XmlSchemaForm.Unqualified)]
+        [XmlArrayItem("Translation", Form = XmlSchemaForm.Unqualified, IsNullable = false)]
+        public TranslationType[] TranslationData
+        {
             get => m_translationDataField;
-            set {
+            set
+            {
                 m_translationDataField = value;
                 RaisePropertyChanged("TranslationData");
             }
         }
-    
-        /// <remarks/>
-        [System.Xml.Serialization.XmlAttributeAttribute()]
-        public string Key {
+
+        /// <remarks />
+        [XmlAttribute]
+        public string Key
+        {
             get => m_keyField;
-            set {
+            set
+            {
                 m_keyField = value;
                 RaisePropertyChanged("Key");
             }
         }
-    
-        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-    
-        protected void RaisePropertyChanged(string propertyName) {
-            System.ComponentModel.PropertyChangedEventHandler propertyChanged = PropertyChanged;
-            propertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }

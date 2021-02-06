3a0cae2bd1e3fc757dd1304720fda89a45c1749b
Lukas Grünwald <gruenwald.lk@outlook.com>
2021-02-06 16:43:56 +0100

    Initial Cleanup

--- PG.StarWarsGame.Localisation/PG.StarWarsGame.Localisation/Data/Config/v1/LocalisationDataType.cs
+++ PG.StarWarsGame.Localisation/PG.StarWarsGame.Localisation/Data/Config/v1/LocalisationDataType.cs
@@ -1,31 +1,46 @@
+// Copyright (c) 2021 Alamo Engine Tools and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
 namespace PG.StarWarsGame.Localisation.Data.Config.v1
 {
-    /// <remarks/>
-    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
-    [System.SerializableAttribute()]
-    [System.Diagnostics.DebuggerStepThroughAttribute()]
-    [System.ComponentModel.DesignerCategoryAttribute("code")]
-    [System.Xml.Serialization.XmlTypeAttribute(Namespace="http://www.example.org/eaw-translation/")]
-    [System.Xml.Serialization.XmlRootAttribute("LocalisationData", Namespace="http://www.example.org/eaw-translation/", IsNullable=false)]
-    public class LocalisationDataType : object, System.ComponentModel.INotifyPropertyChanged {
-    
+    /// <remarks />
+    [ExcludeFromCodeCoverage]
+    [GeneratedCode("xsd", "4.8.3928.0")]
+    [Serializable]
+    [DebuggerStepThrough]
+    [DesignerCategory("code")]
+    [XmlType(Namespace = "http://www.example.org/eaw-translation/")]
+    [XmlRoot("LocalisationData", Namespace = "http://www.example.org/eaw-translation/", IsNullable = false)]
+    public class LocalisationDataType : object, INotifyPropertyChanged
+    {
         private LocalisationType[] m_localisationField;
-    
-        /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute("Localisation", Form=System.Xml.Schema.XmlSchemaForm.Unqualified)]
-        public LocalisationType[] Localisation {
+
+        /// <remarks />
+        [XmlElement("Localisation", Form = XmlSchemaForm.Unqualified)]
+        public LocalisationType[] Localisation
+        {
             get => m_localisationField;
-            set {
+            set
+            {
                 m_localisationField = value;
                 RaisePropertyChanged("Localisation");
             }
         }
-    
-        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-    
-        protected void RaisePropertyChanged(string propertyName) {
-            System.ComponentModel.PropertyChangedEventHandler propertyChanged = PropertyChanged;
-            propertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }

dac6280bd847d4fbeec04796bfa69ff1ffafd0f7
Lukas Grünwald <gruenwald.lk@outlook.com>
2021-02-22 17:45:30 +0100

    Added Utility Unit Test

---
+++ PG.StarWarsGame.Localisation/PG.StarWarsGame.Localisation.Test/Util/LocalisationUtilityTest.cs
@@ -0,0 +1,65 @@
+// Copyright (c) 2021 Alamo Engine Tools and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PG.Commons.Test;
+using PG.Core.Localisation;
+using PG.StarWarsGame.Localisation.Util;
+
+namespace PG.StarWarsGame.Localisation.Test.Util
+{
+    [TestClass]
+    [TestCategory(TestUtility.TEST_TYPE_UTILITY)]
+    public class LocalisationUtilityTest
+    {
+        private class TestAlamoLanguageDefinition : IAlamoLanguageDefinition, IEquatable<IAlamoLanguageDefinition>
+        {
+            public bool Equals(IAlamoLanguageDefinition other)
+            {
+                if (other == null)
+                {
+                    return false;
+                }
+
+                return Culture.Equals(other.Culture) && LanguageIdentifier.Equals(other.LanguageIdentifier,
+                    StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            public TestAlamoLanguageDefinition(string languageIdentifier, CultureInfo culture)
+            {
+                LanguageIdentifier = languageIdentifier;
+                Culture = culture;
+            }
+
+            public string LanguageIdentifier { get; }
+            public CultureInfo Culture { get; }
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("\r\n\t  \r\n")]
+        [DataRow(" ")]
+        public void TryGuessAlamoLanguageDefinitionByIdentifier_Test__IfIdentifierInvalidReturnsFalse(string identifier)
+        {
+            Assert.IsFalse(
+                LocalisationUtility.TryGuessAlamoLanguageDefinitionByIdentifier(identifier,
+                    out IAlamoLanguageDefinition _));
+        }
+
+        [TestMethod]
+        public void TryGuessAlamoLanguageDefinitionByIdentifier_Test__GuessBuiltInCorrectly()
+        {
+            foreach (OfficialLanguage officialLanguage in Enum.GetValues(typeof(OfficialLanguage)))
+            {
+                TestAlamoLanguageDefinition expected = new TestAlamoLanguageDefinition(
+                    officialLanguage.ToAlamoLanguageIdentifierString(),
+                    officialLanguage.ToCultureInfo());
+                Assert.IsTrue(LocalisationUtility.TryGuessAlamoLanguageDefinitionByIdentifier(officialLanguage.ToString().ToLower(), out IAlamoLanguageDefinition actual));
+                Assert.IsTrue(expected.Equals(actual));
+            }
+        }
+    }
+}

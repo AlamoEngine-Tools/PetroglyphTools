0d7808c1c7a85b29390968a04639f586d4326737
Lukas Grünwald <gruenwald.lk@outlook.com>
2021-04-21 11:01:57 +0200

    Project Cleanup

---
+++ PG.Core/PG.Core/Services/DefaultServiceImplementationAttribute.cs
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 Alamo Engine Tools and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PG.Core.Services
+{
+    /// <summary>
+    /// An attribute that can be used on an interface to mark a given <see cref="Type"/> as the default (or fallback) implementation for a service.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface)]
+    [ExcludeFromCodeCoverage]
+    public sealed class DefaultServiceImplementationAttribute : Attribute
+    {
+        public Type DefaultServiceImplementation { get; }
+
+        public DefaultServiceImplementationAttribute([JetBrains.Annotations.NotNull] Type defaultServiceImplementation)
+        {
+            Debug.Assert(typeof(IService).IsAssignableFrom(defaultServiceImplementation), $"The given type {defaultServiceImplementation} is not marked as {nameof(IService)}.");
+            DefaultServiceImplementation = defaultServiceImplementation;
+        }
+    }
+}
---
+++ PG.Core/PG.Core/Services/IService.cs
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Alamo Engine Tools and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+namespace PG.Core.Services
+{
+    /// <summary>
+    /// Marker interface for services.
+    /// </summary>
+    public interface IService
+    {
+    }
+}

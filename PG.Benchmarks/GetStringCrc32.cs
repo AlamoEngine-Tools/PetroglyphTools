59b23b90b1c46204d993952505b72cda12a4adef
AnakinRaW <anakin@gmx-topmail.de>
2023-08-15 14:04:59 +0200

    add benchmark project and improve perf

---
+++ PG.Benchmarks/GetStringCrc32.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using PG.Commons.Services;
+
+[SimpleJob(RuntimeMoniker.Net481)]
+[SimpleJob(RuntimeMoniker.Net80)]
+[MemoryDiagnoser]
+public class GetStringCrc32
+{
+    private static readonly Random Random = new();
+
+    [Params(10, 257)]
+    public int N;
+
+    private readonly Encoding _encoding = Encoding.ASCII;
+    private string _s1 = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _s1 = RandomString(N);
+    }
+
+    [Benchmark(Baseline = true)]
+    public Crc32 Old()
+    {
+        Span<byte> checksum = stackalloc byte[8];
+        System.IO.Hashing.Crc32.Hash(_encoding.GetBytes(_s1), checksum);
+        return new Crc32(checksum);
+    }
+
+    [Benchmark]
+    public unsafe Crc32 Current()
+    {
+        var stringSpan = _s1.AsSpan();
+        var maxByteSize = _encoding.GetMaxByteCount(stringSpan.Length);
+
+        Span<byte> buffer;
+
+        if (maxByteSize > 256)
+        {
+#if NET
+            var b = new byte[maxByteSize].AsSpan();
+            var nb = _encoding.GetBytes(stringSpan, b);
+            buffer = b.Slice(0, nb);
+#else
+            buffer = _encoding.GetBytes(_s1).AsSpan();
+#endif
+        }
+
+        else
+        {
+            var buff = stackalloc byte[maxByteSize];
+            fixed (char* sp = _s1)
+            {
+                var a = _encoding.GetBytes(sp, _s1.Length, buff, maxByteSize);
+                buffer = new Span<byte>(buff, a);
+            }
+        }
+
+        Span<byte> checksum = stackalloc byte[sizeof(Crc32)];
+        System.IO.Hashing.Crc32.Hash(buffer, checksum);
+        return new Crc32(checksum);
+    }
+
+    private static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[Random.Next(s.Length)]).ToArray());
+    }
+}
--- PG.Commons/PG.Commons/Services/ChecksumService.cs
+++ PG.Commons/PG.Commons/Services/ChecksumService.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 using System;
-using System.Buffers;
 using System.Text;
 
 namespace PG.Commons.Services;
@@ -25,17 +24,36 @@ public class ChecksumService : IChecksumService
         if (encoding == null)
             throw new ArgumentNullException(nameof(encoding));
 
-        Span<byte> checksum = stackalloc byte[sizeof(Crc32)];
+        var stringSpan = s.AsSpan();
+        var maxByteSize = encoding.GetMaxByteCount(stringSpan.Length);
+        
+        Span<byte> buffer;
 
-#if NETCOREAPP2_1_OR_GREATER
-        var maxByteSize = encoding.GetMaxByteCount(s.Length);
-        using var pooledMemory = MemoryPool<byte>.Shared.Rent(maxByteSize);
-        var poolBuf = pooledMemory.Memory.Span;
-        var readBytes = encoding.GetBytes(s, poolBuf);
-        var buffer = poolBuf.Slice(0, readBytes);
+        // This threshold is somewhat arbitrary. It should be <= 1MB though. 
+        if (maxByteSize > 256)
+        {
+#if NETSTANDARD2_1_OR_GREATER || NET
+            // This is actually slightly faster than using encoding.GetBytes(s)
+            // Using array pool is significant slower but might safe some allocations. 
+            var b = new byte[maxByteSize].AsSpan();
+            var bytesRead = encoding.GetBytes(stringSpan, b);
+            buffer = b.Slice(0, bytesRead);
 #else
-        var buffer = encoding.GetBytes(s).AsSpan();
+            // I'm afraid this is the best performing we can get for long string in NET Framework.
+            buffer = encoding.GetBytes(s).AsSpan();
 #endif
+        }
+        else
+        {
+            var buff = stackalloc byte[maxByteSize];
+            fixed (char* sp = s)
+            {
+                var bytesRead = encoding.GetBytes(sp, s.Length, buff, maxByteSize);
+                buffer = new Span<byte>(buff, bytesRead);
+            }
+        }
+
+        Span<byte> checksum = stackalloc byte[sizeof(Crc32)];
         System.IO.Hashing.Crc32.Hash(buffer, checksum);
         return new Crc32(checksum);
     }

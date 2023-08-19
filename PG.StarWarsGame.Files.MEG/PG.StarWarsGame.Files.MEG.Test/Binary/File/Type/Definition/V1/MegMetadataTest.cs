1352fdcaf43d8b2d0cca9eaad76fce43d2bab17a
AnakinSklavenwalker <anakin@gmx-topmail.de>
2023-08-19 10:30:09 +0200

    some tests

--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Binary/File/Type/Definition/V1/MegMetadataTest.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Binary/File/Type/Definition/V1/MegMetadataTest.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PG.StarWarsGame.Files.MEG.Binary.Shared.Metadata;
@@ -29,26 +28,6 @@ public class MegMetadataTest
     }
 
     [TestMethod]
-    public void Ctor_Test__ThrowsArgumentException()
-    {
-        var header1 = new MegHeader(1, 1);
-        var header2 = new MegHeader(2, 2);
-        var fileTable1 = new MegFileTable(new List<MegFileContentTableRecord>
-            { new(default, 0, 0, 0, 0) });
-       var fileNameTable1 = new MegFileNameTable(new List<MegFileNameTableRecord>
-            { new("123", Encoding.ASCII) });
-       var fileNameTable2 = new MegFileNameTable(new List<MegFileNameTableRecord>
-           { 
-               new("123", Encoding.ASCII),
-               new("456", Encoding.ASCII)
-           });
-
-        Assert.ThrowsException<ArgumentException>(() => new MegMetadata(header2, fileNameTable1, fileTable1));
-        Assert.ThrowsException<ArgumentException>(() => new MegMetadata(header1, fileNameTable2, fileTable1));
-    }
-
-
-    [TestMethod]
     public void Ctor_Test__Correct()
     {
         new MegMetadata(
@@ -57,34 +36,5 @@ public class MegMetadataTest
             new MegFileTable(new List<MegFileContentTableRecord> { default }));
 
         Assert.IsTrue(true);
-    }
-
-    [TestMethod]
-    public void Test_Size()
-    {
-        var header = new MegHeader(1, 1);
-        var fileNameTable = new MegFileNameTable(new List<MegFileNameTableRecord> { new("123", Encoding.ASCII) });
-        var fileTable = new MegFileTable(new List<MegFileContentTableRecord> { default });
-
-        var metadata = new MegMetadata(header, fileNameTable, fileTable);
-
-        Assert.AreEqual(header.Size + fileNameTable.Size + fileTable.Size, metadata.Size);
-    }
-
-    [TestMethod]
-    public void Test_Bytes()
-    {
-        var header = new MegHeader(1, 1);
-        var fileNameTable = new MegFileNameTable(new List<MegFileNameTableRecord> { new("123", Encoding.ASCII) });
-        var fileTable = new MegFileTable(new List<MegFileContentTableRecord> { default });
-
-        var metadata = new MegMetadata(header, fileNameTable, fileTable);
-
-        var expectedBytes = header.Bytes
-            .Concat(fileNameTable.Bytes)
-            .Concat(fileTable.Bytes)
-            .ToArray();
-
-        CollectionAssert.AreEqual(expectedBytes, metadata.Bytes);
     }
 }

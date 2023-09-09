22a720f2a47c81c0e131d31d7681f0755de9b4cc
AnakinRaW <anakin@gmx-topmail.de>
2023-09-09 15:28:44 +0200

    finish read meg files

--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Binary/MegFileBinaryServiceV1IntegrationTest.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Binary/MegFileBinaryServiceV1IntegrationTest.cs
@@ -1,7 +1,8 @@
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using PG.StarWarsGame.Files.MEG.Binary.V1;
@@ -11,40 +12,72 @@ namespace PG.StarWarsGame.Files.MEG.Test.Binary;
 [TestClass]
 public class MegFileBinaryServiceV1IntegrationTest
 {
-    private IFileSystem _fileSystem;
-
-    private MegFileBinaryServiceV1 _binaryService;
-    private Mock<IServiceProvider> _serviceProviderMock;
+    private MegFileBinaryServiceV1 _binaryService = null!;
 
     [TestInitialize]
     public void SetUp()
     {
-        _fileSystem = new MockFileSystem();
+        var fs = new MockFileSystem();
         var sp = new Mock<IServiceProvider>();
-        sp.Setup(s => s.GetService(typeof(IFileSystem))).Returns(_fileSystem);
-        _serviceProviderMock = sp;
-
+        sp.Setup(s => s.GetService(typeof(IFileSystem))).Returns(fs);
         _binaryService = new MegFileBinaryServiceV1(sp.Object);
-
-        _fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
-        {
-            {
-                MegTestConstants.GetGameObjectFilesPath(),
-                new MockFileData(MegTestConstants.CONTENT_GAMEOBJECTFILES)
-            },
-            {
-                MegTestConstants.GetCampaignFilesPath(),
-                new MockFileData(MegTestConstants.CONTENT_CAMPAIGNFILES)
-            },
-            {
-                MegTestConstants.GetMegFilePath(),
-                new MockFileData(MegTestConstants.CONTENT_MEG_FILE)
-            }
-        });
     }
 
     [TestMethod]
     public void Test__ReadBinary_EmptyMeg()
     {
+        var emptyMeg = GetEmbeddedResource("Files.v1_empty.meg");
+        var megMetadata = _binaryService.ReadBinary(emptyMeg);
+        Assert.AreEqual(0, megMetadata.FileNameTable.Count);
+        Assert.AreEqual(0, megMetadata.FileTable.Count);
+        Assert.AreEqual(0, megMetadata.Header.FileNumber);
+    }
+
+    [TestMethod]
+    public void Test__ReadBinary_OneFile()
+    {
+        var emptyMeg = GetEmbeddedResource("Files.v1_1_file_data.meg");
+        var megMetadata = _binaryService.ReadBinary(emptyMeg);
+        Assert.AreEqual(1, megMetadata.FileNameTable.Count);
+        Assert.AreEqual(1, megMetadata.FileTable.Count);
+        Assert.AreEqual(1, megMetadata.Header.FileNumber);
+
+        var fileSizes = megMetadata.FileTable.Select(x => x.FileSize).Sum(x => x);
+        Assert.AreEqual(3, fileSizes);
+    }
+
+    [TestMethod]
+    public void Test__ReadBinary_TwoFiles()
+    {
+        var emptyMeg = GetEmbeddedResource("Files.v1_2_files_empty.meg");
+        var megMetadata = _binaryService.ReadBinary(emptyMeg);
+        Assert.AreEqual(2, megMetadata.FileNameTable.Count);
+        Assert.AreEqual(2, megMetadata.FileTable.Count);
+        Assert.AreEqual(2, megMetadata.Header.FileNumber);
+
+        var fileSizes = megMetadata.FileTable.Select(x => x.FileSize).Sum(x => x);
+        Assert.AreEqual(0, fileSizes);
+    }
+
+    [TestMethod]
+    public void Test__ReadBinary_TwoFilesWithNonAsciiName()
+    {
+        var emptyMeg = GetEmbeddedResource("Files.v1_2_files_with_extended_ascii_name.meg");
+        var megMetadata = _binaryService.ReadBinary(emptyMeg);
+        Assert.AreEqual(2, megMetadata.FileNameTable.Count);
+        Assert.AreEqual(2, megMetadata.FileTable.Count);
+        Assert.AreEqual(2, megMetadata.Header.FileNumber);
+    }
+
+    [TestMethod]
+    public void Test__ReadBinary_TwoFiles2()
+    {
+        var megMetadata = _binaryService.ReadBinary(new MemoryStream(MegTestConstants.CONTENT_MEG_FILE));
+    }
+
+    private static Stream GetEmbeddedResource(string path)
+    {
+        var currentAssembly = typeof(MegFileBinaryServiceV1IntegrationTest).Assembly;
+        return currentAssembly.GetManifestResourceStream($"{currentAssembly.GetName().Name}.Resources.{path}")!;
     }
 }
--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Binary/MegFileBinaryServiceV1Test.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Binary/MegFileBinaryServiceV1Test.cs
@@ -3,83 +3,219 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using PG.Commons.Binary;
+using PG.Commons.Services;
+using PG.StarWarsGame.Files.MEG.Binary.Metadata;
 using PG.StarWarsGame.Files.MEG.Binary.V1;
+using PG.StarWarsGame.Files.MEG.Binary.V1.Metadata;
 
 namespace PG.StarWarsGame.Files.MEG.Test.Binary;
 
 [TestClass]
 public class MegFileBinaryServiceV1Test
 {
-    private IFileSystem _fileSystem;
-
-    private MegFileBinaryServiceV1 _binaryService;
-    private Mock<IServiceProvider> _serviceProviderMock;
+    private MegFileBinaryServiceV1 _binaryService = null!;
 
     [TestInitialize]
     public void SetUp()
     {
-        _fileSystem = new MockFileSystem();
+        var fs = new MockFileSystem();
         var sp = new Mock<IServiceProvider>();
-        sp.Setup(s => s.GetService(typeof(IFileSystem))).Returns(_fileSystem);
-        _serviceProviderMock = sp;
-
+        sp.Setup(s => s.GetService(typeof(IFileSystem))).Returns(fs);
         _binaryService = new MegFileBinaryServiceV1(sp.Object);
     }
 
+    [TestMethod]
     public void Test__CreateMegMetadata()
     {
+        var header = (MegHeader)default;
+        var nameTable = new Mock<MegFileNameTable>(Array.Empty<MegFileNameTableRecord>());
+        var fileTable = new Mock<MegFileTable>(Array.Empty<MegFileContentTableRecord>());
 
+        var metadata = _binaryService.CreateMegMetadata(header, nameTable.Object, fileTable.Object);
+
+        Assert.AreEqual(header, metadata.Header);
+        Assert.AreEqual(nameTable.Object, metadata.FileNameTable);
+        Assert.AreEqual(fileTable.Object, metadata.FileTable);
     }
 
-    //[TestMethod]
-    //public void FromHolder_Test__FileHeaderIsConsistent()
-    //{
-    //    var megFileHolder = new MegFileHolder(MegTestConstants.GetBasePath(), "test");
-    //    megFileHolder.Content.Add(new MegFileDataEntry("data/xml/campaignfiles.xml",
-    //        MegTestConstants.GetCampaignFilesPath()));
-    //    megFileHolder.Content.Add(new MegFileDataEntry("data/xml/gameobjectfiles.xml",
-    //        MegTestConstants.GetGameObjectFilesPath()));
-    //    var builder = new MegFileBinaryServiceV1(m_fileSystem);
-    //    var megFile = builder.FromHolder(megFileHolder);
-    //    Assert.IsNotNull(megFile);
-    //    Assert.IsNotNull(megFile.Header);
-    //    Assert.IsNotNull(megFile.FileContentTable);
-    //    Assert.IsNotNull(megFile.FileNameTable);
-    //    Assert.AreEqual(2u, megFile.Header.NumFiles);
-    //    Assert.AreEqual(2u, megFile.Header.NumFileNames);
-    //    Assert.AreEqual(40, megFile.FileContentTable.Size);
-    //    Assert.AreEqual(58, megFile.FileNameTable.Size);
-    //    Assert.IsTrue(megFile.FileNameTable[0].FileName
-    //        .Equals("data/xml/gameobjectfiles.xml", StringComparison.InvariantCultureIgnoreCase));
-    //    Assert.IsTrue(megFile.FileNameTable[1].FileName.Equals("data/xml/campaignfiles.xml",
-    //        StringComparison.InvariantCultureIgnoreCase));
-    //}
+    [TestMethod]
+    public void Test__BuildMegHeader_NotSupportedFileCount()
+    {
+        var data = new byte[]
+        {
+            0, 0, 0, 0x80,
+            0, 0, 0, 0x80
+        };
 
-    //[TestMethod]
-    //public void FromHolder_Test__FileHeaderIsBinaryEquivalent()
-    //{
-    //    var megFileHolder = new MegFileHolder(MegTestConstants.GetBasePath(),
-    //        "FromHolder_Test__FileHeaderIsBinaryEquivalent");
-    //    megFileHolder.Content.Add(new MegFileDataEntry("data/xml/campaignfiles.xml",
-    //        MegTestConstants.GetCampaignFilesPath()));
-    //    megFileHolder.Content.Add(new MegFileDataEntry("data/xml/gameobjectfiles.xml",
-    //        MegTestConstants.GetGameObjectFilesPath()));
-    //    var builder = new MegFileBinaryServiceV1(m_fileSystem);
-    //    var megFile = builder.FromHolder(megFileHolder);
-    //    Assert.IsNotNull(megFile);
-    //    m_fileSystem.File.WriteAllBytes(
-    //        m_fileSystem.Path.Combine(MegTestConstants.GetBasePath(),
-    //            megFileHolder.FullyQualifiedName), megFile.ToBytes());
-    //    var readMegFile = m_fileSystem.File.ReadAllBytes(m_fileSystem.Path.Combine(MegTestConstants.GetBasePath(),
-    //        megFileHolder.FullyQualifiedName));
-    //    Assert.AreEqual(MegTestConstants.CONTENT_MEG_FILE_HEADER.Length, readMegFile.Length);
-    //    for (var i = 0; i < MegTestConstants.CONTENT_MEG_FILE_HEADER.Length; i++)
-    //    {
-    //        Assert.AreEqual(MegTestConstants.CONTENT_MEG_FILE_HEADER[i], readMegFile[i]);
-    //    }
-    //}
+        var reader = new BinaryReader(new MemoryStream(data));
+        Assert.ThrowsException<NotSupportedException>(() => _binaryService.BuildMegHeader(reader));
+    }
+
+    [TestMethod]
+    public void Test__BuildMegHeader_NotEqualFile_FileName_Count()
+    {
+        var data = new byte[]
+        {
+            1, 0, 0, 0,
+            2, 0, 0, 0
+        };
+
+        var reader = new BinaryReader(new MemoryStream(data));
+        Assert.ThrowsException<BinaryCorruptedException>(() => _binaryService.BuildMegHeader(reader));
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(HeaderTestData), DynamicDataSourceType.Method)]
+    public void Test__BuildMegHeader_Correct(byte[] data, uint numFiles, uint numNames)
+    {
+        var reader = new BinaryReader(new MemoryStream(data));
+        var header = _binaryService.BuildMegHeader(reader);
+
+        Assert.AreEqual(numFiles, header.NumFiles);
+        Assert.AreEqual(numNames, header.NumFileNames);
+    }
+
+    private static IEnumerable<object[]> HeaderTestData()
+    {
+        return new[]
+        {
+            new object[] { new byte[]
+            { 
+                0,0,0,0,
+                0,0,0,0
+            }, 0u, 0u },
+            new object[] { new byte[]
+            {
+                0,0,0,0,
+                0,0,0,0,
+                1,2,3,4, // Random Junk 
+            }, 0u, 0u },
+            new object[] { new byte[]
+            {
+                1,0,0,0,
+                1,0,0,0
+            }, 1u, 1u },
+            new object[] { new byte[]
+            {
+                0xFF,0xFF,0xFF,0x7F,
+                0xFF,0xFF,0xFF,0x7F
+            }, (uint)int.MaxValue, (uint)int.MaxValue },
+        };
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(FileTableIntegrationTestData), DynamicDataSourceType.Method)]
+    public void Test__BuildFileTable_Integration(
+        uint numberEntries, 
+        byte[] data, 
+        int dataToInspect, 
+        Crc32 crc32,
+        uint fIndex,
+        uint fSize,
+        uint fOffset,
+        uint nIndex)
+    {
+        var header = new MegHeader(numberEntries, numberEntries);
+
+        var reader = new BinaryReader(new MemoryStream(data));
+        var fileTable = _binaryService.BuildFileTable(reader, header);
+
+        Assert.AreEqual((int)numberEntries, fileTable.Count);
+
+        if (fileTable.Count == 0)
+            return;
+
+        var record = fileTable[dataToInspect];
+
+        Assert.AreEqual(crc32, record.Crc32);
+        Assert.AreEqual(fIndex, record.FileTableRecordIndex);
+        Assert.AreEqual(fSize, record.FileSize);
+        Assert.AreEqual(fOffset, record.FileOffset);
+        Assert.AreEqual(nIndex, record.FileNameIndex);
+    }
+
+    private static IEnumerable<object[]> FileTableIntegrationTestData()
+    {
+        return new[]
+        {
+            new object[] { 0u, Array.Empty<byte>(), -1, new Crc32(), 0u,0u,0u,0u},
+            new object[] { 1u, new byte[]
+            {
+                1,0,0,0, // CRC
+                0,0,0,0, // Index (FileTable)
+                2,0,0,0, // Size
+                0x40,0,0,0, // Offset
+                0,0,0,0  // Index (NameTable)
+            }, 0, new Crc32(1), 0u, 2u, 0x40u, 0u},
+            new object[] { 1u, new byte[]
+            {
+                1,0,0,0, // CRC
+                0,0,0,0, // Index (FileTable)
+                2,0,0,0, // Size
+                0x40,0,0,0, // Offset
+                0,0,0,0,  // Index (NameTable)
+
+                1,2,3,4 // Junk
+            },  0, new Crc32(1), 0u, 2u, 0x40u, 0u},
+            new object[] { 2u, new byte[]
+            {
+                1,0,0,0, // CRC
+                0,0,0,0, // Index (FileTable)
+                2,0,0,0, // Size
+                0x40,0,0,0, // Offset
+                0,0,0,0,  // Index (NameTable)
+
+                2,0,0,0, // CRC
+                1,0,0,0, // Index (FileTable)
+                3,0,0,0, // Size
+                0x80,0,0,0, // Offset
+                1,0,0,0  // Index (NameTable)
+            }, 1, new Crc32(2), 1u, 3u, 0x80u, 1u}
+        };
+    }
+
+
+    [DataTestMethod]
+    [DynamicData(nameof(NotSupportedFileTableRecords), DynamicDataSourceType.Method)]
+    public void Test__BuildFileTable_NotSupported(uint numberEntries, byte[] data)
+    {
+        var header = new MegHeader(numberEntries, numberEntries);
+        var reader = new BinaryReader(new MemoryStream(data));
+        Assert.ThrowsException<NotSupportedException>(() => _binaryService.BuildFileTable(reader, header));
+    }
+
+    private static IEnumerable<object[]> NotSupportedFileTableRecords()
+    {
+        return new[]
+        {
+            new object[]
+            {
+                1u, new byte[]
+                {
+                    1, 0, 0, 0, // CRC
+                    0, 0, 0, 0x80, // Index (FileTable)
+                    0, 0, 0, 0, // Size
+                    0, 0, 0, 0, // Offset
+                    0, 0, 0, 0 // Index (NameTable)
+                }
+            },
+            new object[]
+            {
+                1u, new byte[]
+                {
+                    1, 0, 0, 0, // CRC
+                    0, 0, 0, 0, // Index (FileTable)
+                    2, 0, 0, 0, // Size
+                    0, 0, 0, 0, // Offset
+                    0, 0, 0, 0x80 // Index (NameTable)
+                }
+            },
+        };
+    }
 }

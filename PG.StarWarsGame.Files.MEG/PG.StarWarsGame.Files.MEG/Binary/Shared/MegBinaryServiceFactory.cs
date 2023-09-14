17d555763c4daab36bc79202f7129c2ae8a2c860
AnakinRaW <anakin@gmx-topmail.de>
2023-09-14 09:37:35 +0200

    optimize version search a bit

--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/Shared/MegBinaryServiceFactory.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/Shared/MegBinaryServiceFactory.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 using System;
-using System.Net.Http.Headers;
 using PG.StarWarsGame.Files.MEG.Binary.V1;
 using PG.StarWarsGame.Files.MEG.Files;
 
--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Services/MegFileService.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Services/MegFileService.cs
@@ -48,7 +48,7 @@ public sealed class MegFileService : ServiceBase, IMegFileService
     {
         using var fs = FileSystem.FileStream.New(filePath, FileMode.Open, FileAccess.Read);
 
-        var megVersion = GetMegFileVersion(fs, out var encrypted);
+        var megVersion = Services.GetRequiredService<IMegVersionIdentifier>().GetMegFileVersion(fs, out var encrypted);
 
         if (encrypted)
         {
@@ -140,9 +140,7 @@ public sealed class MegFileService : ServiceBase, IMegFileService
     public MegFileVersion GetMegFileVersion(string file, out bool encrypted)
     {
         if (string.IsNullOrWhiteSpace(file))
-        {
             throw new ArgumentNullException(nameof(file));
-        }
 
         using var fs = FileSystem.FileStream.New(file, FileMode.Open, FileAccess.Read, FileShare.Read);
         return GetMegFileVersion(fs, out encrypted);
@@ -151,167 +149,6 @@ public sealed class MegFileService : ServiceBase, IMegFileService
     /// <inheritdoc />
     public MegFileVersion GetMegFileVersion(Stream stream, out bool encrypted)
     {
-        if (stream == null)
-        {
-            throw new ArgumentNullException(nameof(stream));
-        }
-
-        if (!stream.CanRead || !stream.CanSeek)
-        {
-            throw new ArgumentException("Stream must be readable and seekable.", nameof(stream));
-        }
-
-        encrypted = false;
-
-        using var reader = new BinaryReader(stream, Encoding.UTF8, true);
-
-        var flags = reader.ReadUInt32();
-        var id = reader.ReadUInt32();
-
-
-        // In V2 and V3 id and flags are never equal, where they are in V1.
-        // (However this is an assumption my Mike.NL)
-        // Note: In V1 we *could* have the situation where we store as many files in the meg to coincidentally match the magic number.
-        // Thus we don't check for the magic number as that it would not gain us anything.
-        if (flags == id)
-        {
-            if (flags == 0)
-            {
-                throw new InvalidOperationException("Empty .MEG files re not supported");
-            }
-
-            return MegFileVersion.V1;
-        }
-
-
-        if (id != MegFileConstants.MegFileMagicNumber)
-        {
-            throw new BinaryCorruptedException("Unrecognized .MEG file version");
-        }
-
-        // This file is encrypted, thus it can only be V3
-        if (flags == MegFileConstants.MegFileEncryptedFlag)
-        {
-            encrypted = true;
-            return MegFileVersion.V3;
-        }
-
-
-        var dataStart = reader.ReadUInt32();
-        var numFilenames = reader.ReadUInt32();
-        var numFiles = reader.ReadUInt32();
-
-        Debug.Assert(numFiles == numFilenames);
-
-        if (numFiles == 0)
-        {
-            throw new InvalidOperationException("Empty .MEG files re not supported");
-        }
-
-        // So far the file could be either V2 or V3. 
-        // Efficient approach to check the meg version: 
-        // For non empty meg files, we know the FileTable always starts at
-        //
-        //      fileTableStart = dataStart - n * 20     where n stands for the number of files. 
-        //                                              The record size of the FileTable for V2 and V3 (unencrypted)
-        //                                              is always (5 * 4 bytes) = 20 bytes long.
-        //
-        // So we simply assume we are dealing with a V3 meg file.
-        // In that case, reading the next uint32 should yield us the filenamesSize.
-        // There are several condition to check now.
-        //
-        // 1)   If that read operation failed
-        //          --> we must have a V2 meg file. 
-        //
-        // 2)   If that read operation passed, we still could have either version. We now check whether
-        //
-        //          24 bytes + filenamesSize       where 24 bytes is
-        //                                         the size of the V3 header
-        //
-        //      equals the value of fileTableStart.
-        //      If the values do not match we are a V2 case where the reading filenamesSize gave us some garbage.
-        //      However, we could end up in a case where "the garbage" randomly caused the equation to match but we still have a V2 input file. 
-        //      Thus we parse the first and the last element of the FileTable and check its data for plausibility where
-        //
-        //          flags       uint16      0
-        //          crc32       uint32      (omitted)
-        //          index       uint32      0 OR N - 1
-        //      Only if the check passes we return a V3 version.
-        //
-        //      Note: While this also is no *proven* approach the chances are towards 0 we end up having a false positive
-        if (flags == MegFileConstants.MegFileUnencryptedFlag)
-        {
-            uint filenamesSize;
-
-            if (!TryReadUInt32(reader, out filenamesSize))
-            {
-                return MegFileVersion.V2;
-            }
-
-            // known start of the FileTable
-            var fileTableOffset = dataStart - numFiles * 20;
-
-
-            if (fileTableOffset != 24 + filenamesSize)
-            {
-                return MegFileVersion.V2;
-            }
-
-            // Now check for first and last element values
-
-            reader.BaseStream.Position = fileTableOffset;
-
-            if (!FileRecordIsV3(reader, 0))
-            {
-                return MegFileVersion.V2;
-            }
-
-            if (numFiles == 1)
-            {
-                return MegFileVersion.V3;
-            }
-
-            var lastFileRecordPosition = dataStart - 20;
-            reader.BaseStream.Position = lastFileRecordPosition;
-            var lastFileTableIndex = numFiles - 1;
-
-            return FileRecordIsV3(reader, lastFileTableIndex) ? MegFileVersion.V3 : MegFileVersion.V2;
-        }
-
-        throw new BinaryCorruptedException("Unrecognized .MEG file version");
-    }
-
-    private static bool FileRecordIsV3(BinaryReader reader, uint expectedIndex)
-    {
-        try
-        {
-            var flag = reader.ReadUInt16();
-            if (flag != 0)
-            {
-                return false;
-            }
-
-            reader.ReadUInt32(); // CRC
-            var index = reader.ReadUInt32();
-            return index == expectedIndex;
-        }
-        catch
-        {
-            return false;
-        }
-    }
-
-    private static bool TryReadUInt32(BinaryReader reader, out uint value)
-    {
-        try
-        {
-            value = reader.ReadUInt32();
-            return true;
-        }
-        catch (EndOfStreamException)
-        {
-            value = 0;
-            return false;
-        }
+        return Services.GetRequiredService<IMegVersionIdentifier>().GetMegFileVersion(stream, out encrypted);
     }
 }

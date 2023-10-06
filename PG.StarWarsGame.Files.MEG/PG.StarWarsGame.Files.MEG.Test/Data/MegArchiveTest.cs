59e11c8c3d7baff2106a680323c649f5134fb63e
AnakinSklavenwalker <anakin@gmx-topmail.de>
2023-10-06 19:00:04 +0200

    test globbing

--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Data/MegArchiveTest.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Data/MegArchiveTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using DotNet.Globbing;
-using Microsoft.Extensions.FileSystemGlobbing;
+﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PG.Commons.Services;
 using PG.StarWarsGame.Files.MEG.Data;
@@ -13,57 +9,30 @@ namespace PG.StarWarsGame.Files.MEG.Test.Data;
 public class MegArchiveTest
 {
     [TestMethod]
-    public void Test_FindAllEntries_FindAny()
-    {
-        var entry = CreateFromFile("test.txt");
-
-        var a = new MegArchive(new List<MegFileDataEntry>
-        {
-            entry
-        });
-
-        var entries = a.FindAllEntries("*").ToList();
-
-        CollectionAssert.Contains(entries, entry);
-
-
-        //Glob g = Glob.Parse("new/**/test.txt", new GlobOptions{ Evaluation = new EvaluationOptions {CaseInsensitive = true}});
-
-        //var r = g.IsMatch("New/../Test.txt");
-
-        //var files = new List<string>{"Test.txt", "New/../Test.txt", "new/test.txt", "x:/test.txt"};
-
-        //var fs = new InMemoryDirectoryInfo("c:", files);
-
-        //var f = fs.EnumerateFileSystemInfos().ToList();
-
-        //var m = new Matcher(StringComparison.InvariantCultureIgnoreCase);
-        //m.AddInclude("**/test.txt");
-        //var re = m.Execute(fs).Files.ToList();
-    }
-
-    [TestMethod]
-    public void Test_FindAllEntries_Empty()
-    {
-        var a = new MegArchive(new List<MegFileDataEntry>
-        {
-            Capacity = 0
-        });
-
-        var entries = a.FindAllEntries("*").ToList();
-        Assert.AreEqual(0, entries.Count);
-    }
-
-    [TestMethod]
-    [DataRow("", new[] { "", "" }, new[] { "", "" })]
+    [DataRow("*",             new string[] {  },                                                       new string[] { })]
+    [DataRow("*",             new[] { "a.txt", "new/a.txt" },                                          new[] { "a.txt" })]
+    [DataRow("**/*",          new[] { "a.txt", "new/a.txt", "c:/a.txt" },                              new[] { "a.txt", "new/a.txt", "c:/a.txt" })]
+    [DataRow("**/*.xml",      new[] { "a.txt", "a.xml", "new/a.txt", "xml/a.xml" },                    new[] { "a.xml", "xml/a.xml" })]
+    [DataRow("*_eng.mp3",     new[] { "a.mp3", "a_eng.mp3", "a_ger.mp3", "b_eng.mp3", "b_eng.mp2" },   new[] { "a_eng.mp3", "b_eng.mp3" })]
+    [DataRow("a*_eng.mp3",    new[] { "a_eng.mp3", "ab_eng.mp3", "a_ger.mp3", "b_eng.mp3" },           new[] { "a_eng.mp3", "ab_eng.mp3" })]
+    [DataRow("**/a.*",        new[] { "a.txt", "a.xml", "new/b.txt", "xml/a.xml" },                    new[] { "a.txt", "a.xml", "xml/a.xml" })]
+    [DataRow("a.txt",         new[] { "a.txt", "a.xml", "new/a.txt", "c:/a.txt" },                     new[] { "a.txt" })]
+    [DataRow("a.txt",         new[] { "a.xml" },                                                       new string[] { })]
+    [DataRow("**/a.txt",      new[] { "a.txt", "b.txt", "new/a.txt" },                                 new[] { "a.txt", "new/a.txt" })]
+    [DataRow("**/new/a.txt",  new[] { "a.txt", "c:/new/a.txt", "new/a.txt" },                          new[] { "c:/new/a.txt", "new/a.txt" })]
+    // Checks case insensitivity
+    [DataRow("NEW/a.txt", new[] { "a.txt", "a.xml", "new/a.txt" }, new[] { "new/a.txt" })]
+    // Below are unusual cases and the behavior is highly dependent on the library used for globbing
+    // On a real file system, this is a false positive ("new/../a.txt" should NOT be present)
+    // and also this is a false negative ("./a.txt" should be present)
+    [DataRow("a.txt",         new[] { "./a.txt", "new/../a.txt" },                                     new string[] { })]
+    // On a real file system, this is a false positive ("new/../a.txt" should NOT be present)
+    [DataRow("new/**/a.txt",  new[] { "new/../a.txt", "new/././a.txt" },                               new[] { "new/../a.txt", "new/././a.txt" })] 
     public void Test_FindAllEntries(string pattern, string[] files, string[] expectedMatches)
     {
         var megFiles = files.Select(CreateFromFile).ToList();
-
-        var a = new MegArchive(megFiles);
-
-        var entries = a.FindAllEntries(pattern).Select(e => e.FilePath).ToList();
-
+        var meg = new MegArchive(megFiles);
+        var entries = meg.FindAllEntries(pattern).Select(e => e.FilePath).ToList();
         CollectionAssert.AreEqual(expectedMatches, entries);
     }
 

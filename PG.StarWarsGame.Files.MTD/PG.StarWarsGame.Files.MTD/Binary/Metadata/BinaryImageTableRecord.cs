8122d267479278d862d9d76091b30fa81cac3167
AnakinRaW <anakin@gmx-topmail.de>
2024-03-11 12:12:47 +0100

    start from scratch

---
+++ PG.StarWarsGame.Files.MTD/PG.StarWarsGame.Files.MTD/Binary/Metadata/BinaryImageTableRecord.cs
@@ -0,0 +1,22 @@
+﻿using PG.Commons.Binary;
+
+namespace PG.StarWarsGame.Files.MTD.Binary.Metadata;
+
+internal struct BinaryImageTableRecord : IBinary
+{
+    string Name { get; }
+
+    public uint X { get; }
+
+    public uint Y { get; }
+
+    public uint Width { get; }
+
+    public uint Height { get; }
+
+    public bool Alpha { get; }
+
+    public byte[] Bytes { get; }
+
+    public int Size { get; }
+}

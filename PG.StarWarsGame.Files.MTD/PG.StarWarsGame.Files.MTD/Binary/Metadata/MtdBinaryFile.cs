514f2c979f92077cd79321805b85e9db6f413942
AnakinRaW <anakin@gmx-topmail.de>
2024-03-11 12:12:47 +0100

    start from scratch

---
+++ PG.StarWarsGame.Files.MTD/PG.StarWarsGame.Files.MTD/Binary/Metadata/MtdBinaryFile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PG.Commons.Binary;
+
+namespace PG.StarWarsGame.Files.MTD.Binary.Metadata;
+
+internal class MtdBinaryFile : BinaryBase, IBinaryTable<BinaryImageTableRecord>
+{
+    public int Count { get; }
+
+    public BinaryImageTableRecord this[int index] => throw new System.NotImplementedException();
+
+    protected override int GetSizeCore()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override byte[] ToBytesCore()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerator<BinaryImageTableRecord> GetEnumerator()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}

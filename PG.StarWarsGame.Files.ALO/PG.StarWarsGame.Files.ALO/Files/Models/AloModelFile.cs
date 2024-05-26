930c9de108c5eed91034d470812db393c06fbfba
AnakinRaW <anakin@gmx-topmail.de>
2024-05-26 11:12:58 +0200

    some checks

--- PG.StarWarsGame.Files.ALO/PG.StarWarsGame.Files.ALO/Files/Models/AloModelFile.cs
+++ PG.StarWarsGame.Files.ALO/PG.StarWarsGame.Files.ALO/Files/Models/AloModelFile.cs
@@ -4,8 +4,13 @@ using PG.StarWarsGame.Files.ALO.Data;
 
 namespace PG.StarWarsGame.Files.ALO.Files.Models;
 
-public sealed class AloModelFile(
-    AlamoModel model,
-    AloFileInformation fileInformation,
-    IServiceProvider serviceProvider)
-    : PetroglyphFileHolder<AlamoModel, AloFileInformation>(model, fileInformation, serviceProvider), IAloModelFile;
+public sealed class AloModelFile : PetroglyphFileHolder<AlamoModel, AloFileInformation>, IAloModelFile
+{
+    public AloModelFile(AlamoModel model,
+        AloFileInformation fileInformation,
+        IServiceProvider serviceProvider) : base(model, fileInformation, serviceProvider)
+    {
+        if (fileInformation.IsParticle)
+            throw new ArgumentException("file information is referencing a particle.", nameof(fileInformation));
+    }
+}
--- PG.StarWarsGame.Files.ALO/PG.StarWarsGame.Files.ALO/Files/Particles/AloParticleFile.cs
+++ PG.StarWarsGame.Files.ALO/PG.StarWarsGame.Files.ALO/Files/Particles/AloParticleFile.cs
@@ -4,8 +4,13 @@ using PG.StarWarsGame.Files.ALO.Data;
 
 namespace PG.StarWarsGame.Files.ALO.Files.Particles;
 
-public sealed class AloParticleFile(
-    AlamoParticle particle,
-    AloFileInformation fileInformation,
-    IServiceProvider serviceProvider)
-    : PetroglyphFileHolder<AlamoParticle, AloFileInformation>(particle, fileInformation, serviceProvider), IAloParticleFile;
+public sealed class AloParticleFile : PetroglyphFileHolder<AlamoParticle, AloFileInformation>, IAloParticleFile
+{
+    public AloParticleFile(AlamoParticle particle,
+        AloFileInformation fileInformation,
+        IServiceProvider serviceProvider) : base(particle, fileInformation, serviceProvider)
+    {
+        if (fileInformation.IsModel)
+            throw new ArgumentException("file information is referencing a model.", nameof(fileInformation));
+    }
+}

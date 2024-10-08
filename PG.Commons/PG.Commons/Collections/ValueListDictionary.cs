052f3ab2d672bb4bfbea0356ed3e6e5dfaf2f89e
AnakinRaW <anakin@gmx-topmail.de>
2024-10-08 16:20:05 +0200

    add tests

--- PG.Commons/PG.Commons/Collections/ValueListDictionary.cs
+++ PG.Commons/PG.Commons/Collections/ValueListDictionary.cs
@@ -8,131 +8,12 @@ using AnakinRaW.CommonUtilities.Collections;
 
 namespace PG.Commons.Collections;
 
-/// <summary>
-/// Represents a generic read-only collection of key/value-list pairs.
-/// </summary>
-/// <typeparam name="TKey">The type of the keys in the read-only dictionary.</typeparam>
-/// <typeparam name="TValue">The type of the value-list in the read-only dictionary.</typeparam>
-public interface IReadOnlyValueListDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>> where TKey : notnull
-{
-    /// <summary>
-    /// Gets an enumerable collection that contains the values in the dictionary.
-    /// </summary>
-    /// <remarks>
-    /// Modifications to the collection are not reflected in the dictionary.
-    /// <br/>
-    /// The order of the keys in the returned <see cref="ICollection{T}"/> is unspecified,
-    /// but it is guaranteed that values of the same key get returned as a sequence in the order they were added.
-    /// </remarks>
-    ICollection<TValue> Values { get; }
-
-    /// <summary>
-    /// Gets an <see cref="ICollection{T}"/> containing the keys in the dictionary.
-    /// </summary>
-    /// <remarks>
-    /// Modifications to the collection are not reflected in the dictionary.
-    /// <br/>
-    /// The order of the keys in the returned <see cref="ICollection{T}"/> is unspecified.
-    /// </remarks>
-    ICollection<TKey> Keys { get; }
-
-    /// <summary>
-    /// Gets the number of values contained in the dictionary.
-    /// </summary>
-    int Count { get; }
-
-    /// <summary>
-    /// Gets the key at the specified index.
-    /// </summary>
-    /// <param name="index">The zero-based index of the key to get.</param>
-    /// <returns>The key at the specified index.</returns>
-    /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/> is not a valid index in the dictionary.</exception>
-    TKey this[int index] { get; }
-
-    /// <summary>
-    /// Gets the value at the specified key index.
-    /// </summary>
-    /// <param name="index">The zero-based index of the key to get the value for.</param>
-    /// <returns>The value at the specified key index.</returns>
-    /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/> is not a valid index in the dictionary.</exception>
-    TValue GetValueAtKeyIndex(int index);
-
-    /// <summary>
-    /// Determines whether the dictionary contains the specified key.
-    /// </summary>
-    /// <param name="key">The key to locate in the dictionary.</param>
-    /// <returns><see langword="true"/> if the dictionary contains the key; otherwise, <see langword="false"/>.</returns>
-    bool ContainsKey(TKey key);
-
-    /// <summary>
-    /// Get the list of values stored at the specified key.
-    /// </summary>
-    /// <param name="key">The key to get the list of values for.</param>
-    /// <returns>The list of values of the specified <paramref name="key"/>.</returns>
-    ReadOnlyFrugalList<TValue> GetValues(TKey key);
-
-    /// <summary>
-    /// Gets the last element with the specified key.
-    /// </summary>
-    /// <param name="key">The key of the element to get.</param>
-    /// <returns>The last element with the specified key.</returns>
-    TValue GetLastValue(TKey key);
-
-    /// <summary>
-    /// Gets the first element with the specified key.
-    /// </summary>
-    /// <param name="key">The key of the element to get.</param>
-    /// <returns>The first element with the specified key.</returns>
-    TValue GetFirstValue(TKey key);
-
-    /// <summary>
-    /// Gets the first value associated with the specified key.
-    /// </summary>
-    /// <param name="key">The key whose value to get.</param>
-    /// <param name="value">
-    /// When this method returns, the first value associated with the specified key, if the key is found;
-    /// otherwise, the default value for the type of the <paramref name="value"/> parameter. This parameter is passed uninitialized.</param>
-    /// <returns><see langword="true"/> if the dictionary contains a value with the specified key; otherwise, <see langword="false"/>.</returns>
-    bool TryGetFirstValue(TKey key, [NotNullWhen(true)] out TValue value);
-
-    /// <summary>
-    /// Gets the last value associated with the specified key.
-    /// </summary>
-    /// <param name="key">The key whose value to get.</param>
-    /// <param name="value">
-    /// When this method returns, the last value associated with the specified key, if the key is found;
-    /// otherwise, the default value for the type of the <paramref name="value"/> parameter. This parameter is passed uninitialized.</param>
-    /// <returns><see langword="true"/> if the dictionary contains a value with the specified key; otherwise, <see langword="false"/>.</returns>
-    bool TryGetLastValue(TKey key, [NotNullWhen(true)] out TValue value);
-
-    /// <summary>
-    /// Gets the list of values associated with the specified key.
-    /// </summary>
-    /// <param name="key">The key whose value to get.</param>
-    /// <param name="values">
-    /// When this method returns, a list of values associated with the specified key, if the key is found;
-    /// otherwise, an empty list. This parameter is passed uninitialized.</param>
-    /// <returns><see langword="true"/> if the dictionary contains at least one value with the specified key; otherwise, <see langword="false"/>.</returns>
-    bool TryGetValues(TKey key, out ReadOnlyFrugalList<TValue> values);
-}
-
-/// <summary>
-/// Represents a generic collection of key/value-list pairs.
-/// </summary>
-/// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
-/// <typeparam name="TValue">The type of the value-list in the dictionary.</typeparam>
-public interface IValueListDictionary<TKey, TValue> : IReadOnlyValueListDictionary<TKey, TValue> where TKey : notnull
-{
-    /// <summary>
-    /// Adds an element with the provided key to the value-list of the dictionary.
-    /// </summary>
-    /// <param name="key">The object to use as the key of the element to add.</param>
-    /// <param name="value">The object to use as the value of the element to add.</param>
-    /// <returns><see langword="true"/> if values of the same <paramref name="key"/> already existed; otherwise, <see langword="false"/>.</returns>
-    bool Add(TKey key, TValue value);
-}
-
-// NOT THREAD-SAFE!
+/// <inheritdoc />
+/// <remarks>
+/// A <see cref="ValueListDictionary{TKey,TValue}"/> can support multiple readers concurrently, as long as the collection is not modified.
+/// Even so, enumerating through a collection is intrinsically not a thread-safe procedure.
+/// In the rare case where an enumeration contends with write accesses, the collection must be locked during the entire enumeration. 
+/// </remarks>
 public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TValue> where TKey : notnull
 {
     private readonly List<TKey> _insertionTrackingList = new();
@@ -141,18 +22,29 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
 
     private readonly IEqualityComparer<TKey> _equalityComparer;
 
+    /// <inheritdoc />
     public int Count => _insertionTrackingList.Count;
 
+    /// <inheritdoc />
     public TKey this[int index] => _insertionTrackingList[index];
 
-    public ICollection<TKey> Keys => _singleValueDictionary.Keys.Concat(_multiValueDictionary.Keys).ToList();
+    /// <inheritdoc />
+    public virtual ICollection<TKey> Keys => _singleValueDictionary.Keys.Concat(_multiValueDictionary.Keys).ToList();
 
+    /// <inheritdoc />
     public ICollection<TValue> Values => this.Select(x => x.Value).ToList();
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValueListDictionary{TKey,TValue}"/> class that is empty and uses the default equality comparer for the key type.
+    /// </summary>
     public ValueListDictionary() : this(null)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValueListDictionary{TKey,TValue}"/> class that is empty and uses the specified <see cref="IEqualityComparer"/>.
+    /// </summary>
+    /// <param name="comparer">The <see cref="IEqualityComparer"/> implementation to use when comparing keys, or null to use the default <see cref="IEqualityComparer"/> for the type of the key.</param>
     public ValueListDictionary(IEqualityComparer<TKey>? comparer)
     {
         _equalityComparer = comparer ?? EqualityComparer<TKey>.Default;
@@ -160,6 +52,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
         _multiValueDictionary = new Dictionary<TKey, List<TValue>>(_equalityComparer);
     }
 
+    /// <inheritdoc />
     public TValue GetValueAtKeyIndex(int index)
     {
         if (index < 0 || index >= Count)
@@ -185,11 +78,13 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
         return _multiValueDictionary[key][keyCount - 1];
     }
 
+    /// <inheritdoc />
     public bool ContainsKey(TKey key)
     {
         return _singleValueDictionary.ContainsKey(key) || _multiValueDictionary.ContainsKey(key);
     }
-    
+
+    /// <inheritdoc />
     public bool Add(TKey key, TValue value)
     {
         if (key is null)
@@ -222,6 +117,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
         return true;
     }
 
+    /// <inheritdoc />
     public TValue GetLastValue(TKey key)
     {
         if (_singleValueDictionary.TryGetValue(key, out var value))
@@ -233,6 +129,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
         throw new KeyNotFoundException($"The key '{key}' was not found.");
     }
 
+    /// <inheritdoc />
     public TValue GetFirstValue(TKey key)
     {
         if (_singleValueDictionary.TryGetValue(key, out var value))
@@ -243,7 +140,8 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
 
         throw new KeyNotFoundException($"The key '{key}' was not found.");
     }
-    
+
+    /// <inheritdoc />
     public ReadOnlyFrugalList<TValue> GetValues(TKey key)
     {
         if (TryGetValues(key, out var values)) 
@@ -253,6 +151,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
 
     }
 
+    /// <inheritdoc />
     public bool TryGetFirstValue(TKey key, [NotNullWhen(true)] out TValue value)
     {
         if (_singleValueDictionary.TryGetValue(key, out value!))
@@ -267,6 +166,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
         return false;
     }
 
+    /// <inheritdoc />
     public bool TryGetLastValue(TKey key, [NotNullWhen(true)] out TValue value)
     {
         if (_singleValueDictionary.TryGetValue(key, out value!))
@@ -281,6 +181,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
         return false;
     }
 
+    /// <inheritdoc />
     public bool TryGetValues(TKey key, out ReadOnlyFrugalList<TValue> values)
     {
         if (_singleValueDictionary.TryGetValue(key, out var value))
@@ -299,6 +200,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
         return false;
     }
 
+    /// <inheritdoc />
     public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
     {
         return new Enumerator(this);
@@ -309,6 +211,9 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
         return GetEnumerator();
     }
 
+    /// <summary>
+    /// Enumerates the values of a <see cref="ValueListDictionary{TKey,TValue}"/>.
+    /// </summary>
     public struct Enumerator : IEnumerator<KeyValuePair<TKey, TValue>>
     {
         private Dictionary<TKey, TValue>.Enumerator _singleEnumerator;
@@ -322,6 +227,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
             _multiEnumerator = valueListDictionary._multiValueDictionary.GetEnumerator();
         }
 
+        /// <inheritdoc />
         public KeyValuePair<TKey, TValue> Current =>
             _isMultiEnumeratorActive
                 ? new KeyValuePair<TKey, TValue>(_multiEnumerator.Current.Key, _currentListEnumerator.Current)
@@ -329,6 +235,7 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
 
         object IEnumerator.Current => Current;
 
+        /// <inheritdoc />
         public bool MoveNext()
         {
             if (_singleEnumerator.MoveNext())
@@ -351,118 +258,17 @@ public class ValueListDictionary<TKey, TValue> : IValueListDictionary<TKey, TVal
             return false;
         }
 
+        /// <inheritdoc />
         public void Reset()
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
             _singleEnumerator.Dispose();
             _multiEnumerator.Dispose();
         }
-    }
-}
-
-public static class ValueListDictionaryExtensions
-{
-    public static IEnumerable<T> 
-        AggregateValues<TKey, TValue, T>(
-        this IReadOnlyValueListDictionary<TKey, TValue> valueListDictionary,
-        AggregateStrategy aggregateStrategy,
-        Predicate<T>? filter = null)
-        where TKey : notnull
-        where T : TValue
-    {
-        return valueListDictionary.AggregateValues(valueListDictionary.Keys, aggregateStrategy, filter);
-    }
-
-    public static IEnumerable<T> AggregateValues<TKey, TValue, T>(
-        this IReadOnlyValueListDictionary<TKey, TValue> valueListDictionary,
-        ICollection<TKey> keys,
-        AggregateStrategy aggregateStrategy,
-        Predicate<T>? filter = null)
-        where TKey : notnull 
-        where T : TValue
-    {
-        foreach (var key in keys)
-        {
-            if (!valueListDictionary.ContainsKey(key))
-                continue;
-            if (aggregateStrategy == AggregateStrategy.MultipleValuesPerKey)
-            {
-                foreach (var value in valueListDictionary.GetValues(key))
-                {
-                    if (value is not null)
-                    {
-                        var typedValue = (T)value;
-                        if (filter is not null && filter(typedValue))
-                            yield return typedValue;
-                    }
-
-                }
-            }
-            else
-            {
-                var value = aggregateStrategy == AggregateStrategy.FirstValuePerKey
-                    ? valueListDictionary.GetFirstValue(key)
-                    : valueListDictionary.GetLastValue(key);
-                if (value is not null)
-                {
-                    var typedValue = (T)value;
-                    if (filter is not null && filter(typedValue))
-                        yield return typedValue;
-                }
-            }
-        }
-    }
-
-    public static IEnumerable<T> AggregateValues<TKey, TValue, T>(
-        this IReadOnlyValueListDictionary<TKey, TValue> valueListDictionary,
-        Func<KeyValuePair<TKey, TValue>, T> selector,
-        AggregateStrategy aggregateStrategy)
-        where TKey : notnull
-    {
-        return valueListDictionary.AggregateValues(valueListDictionary.Keys, selector, aggregateStrategy);
-    }
-
-    public static IEnumerable<T> AggregateValues<TKey, TValue, T>(
-        this IReadOnlyValueListDictionary<TKey, TValue> valueListDictionary,
-        ICollection<TKey> keys,
-        Func<KeyValuePair<TKey, TValue>, T> selector,
-        AggregateStrategy aggregateStrategy)
-        where TKey : notnull
-    {
-        foreach (var key in keys)
-        {
-            if (!valueListDictionary.ContainsKey(key))
-                continue;
-            if (aggregateStrategy == AggregateStrategy.MultipleValuesPerKey)
-            {
-                foreach (var value in valueListDictionary.GetValues(key))
-                {
-                    if (value is not null)
-                        yield return selector(new KeyValuePair<TKey, TValue>(key, value));
-
-                }
-            }
-            else
-            {
-                var value = aggregateStrategy == AggregateStrategy.FirstValuePerKey
-                    ? valueListDictionary.GetFirstValue(key)
-                    : valueListDictionary.GetLastValue(key);
-                if (value is not null)
-                {
-                    yield return selector(new KeyValuePair<TKey, TValue>(key, value));
-                }
-            }
-        }
-    }
-
-    public enum AggregateStrategy
-    {
-        FirstValuePerKey,
-        LastValuePerKey,
-        MultipleValuesPerKey,
     }
 }

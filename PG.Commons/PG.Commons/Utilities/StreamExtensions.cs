2ce6db579a29658f68d54db116b7c613de345453
AnakinRaW <anakin@gmx-topmail.de>
2026-01-25 15:36:29 +0100

    add TryGetFilePath overloads and update StreamExtensions to improve path retrieval handling

--- PG.Commons/PG.Commons/Utilities/StreamExtensions.cs
+++ PG.Commons/PG.Commons/Utilities/StreamExtensions.cs
@@ -3,6 +3,7 @@
 
 using PG.Commons.Data;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.IO.Abstractions;
 
@@ -21,7 +22,7 @@ public static class StreamExtensions
     /// <exception cref="InvalidOperationException"><paramref name="stream"/> does not have path information.</exception>
     public static string GetFilePath(this Stream stream)
     {
-        return GetFilePath(stream, out _);
+        return stream.GetFilePath(out _);
     }
 
     /// <summary>
@@ -33,17 +34,72 @@ public static class StreamExtensions
     /// <exception cref="InvalidOperationException"><paramref name="stream"/> does not have path information.</exception>
     public static string GetFilePath(this Stream stream, out bool isMegStream)
     {
+        return !stream.TryGetFilePath(out var filePath, out isMegStream)
+            ? throw new InvalidOperationException("Unable to get file path from Stream")
+            : filePath;
+    }
+
+    /// <summary>
+    /// Attempts to retrieve the file path of the file opened in the specified <see cref="Stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream to retrieve the file path from.</param>
+    /// <returns>
+    /// The file path of the opened file if the operation was successful; otherwise, <see langword="null"/>.
+    /// </returns>
+    public static string? TryGetFilePath(this Stream stream)
+    {
+        return stream.TryGetFilePath(out var fileName, out _) ? fileName : null;
+    }
+
+    /// <summary>
+    /// Attempts to retrieve the file path of the file opened in the specified <see cref="Stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream to retrieve the file path from.</param>
+    /// <param name="fileName">
+    /// When this method returns, contains the file path of the opened file if the operation was successful; 
+    /// otherwise, <see langword="null"/>. This parameter is passed uninitialized.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if the file path was successfully retrieved; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool TryGetFilePath(this Stream stream, [NotNullWhen(true)] out string? fileName)
+    {
+        return stream.TryGetFilePath(out fileName, out _);
+    }
+
+    /// <summary>
+    /// Attempts to retrieve the file path of the file opened in the specified <see cref="Stream"/>.
+    /// </summary>
+    /// <param name="stream">The stream to retrieve the file path from.</param>
+    /// <param name="fileName">
+    /// When this method returns, contains the file path of the opened file if the operation was successful; 
+    /// otherwise, <see langword="null"/>. This parameter is passed uninitialized.
+    /// </param>
+    /// <param name="isMegStream">
+    /// When this method returns, contains a value indicating whether the stream is a MEG file data stream.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if the file path was successfully retrieved; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool TryGetFilePath(this Stream stream, [NotNullWhen(true)] out string? fileName, out bool isMegStream)
+    {
         isMegStream = false;
-        if (stream is FileStream fileStream)
-            return fileStream.Name;
-        if (stream is FileSystemStream fileSystemStream)
-            return fileSystemStream.Name;
-        if (stream is IMegFileDataStream megFileDataStream)
+        switch (stream)
         {
-            isMegStream = true;
-            return megFileDataStream.EntryPath;
+            case FileStream fileStream:
+                fileName =  fileStream.Name;
+                break;
+            case FileSystemStream fileSystemStream:
+                fileName = fileSystemStream.Name;
+                break;
+            case IMegFileDataStream megFileDataStream:
+                isMegStream = true;
+                fileName =  megFileDataStream.EntryPath;
+                break;
+            default:
+                fileName = null;
+                return false;
         }
-        
-        throw new InvalidOperationException("Unable to get file path from Stream");
+        return true;
     }
 }

913b47bf0ad375e9fc653763f6fcdf1dc5b1cd5f
AnakinRaW <anakin@gmx-topmail.de>
2026-01-25 16:21:47 +0100

    extend enumerator tests to include IEnumerator.Current assertions

--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Binary/Metadata/MegFileTableBaseTest.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG.Test/Binary/Metadata/MegFileTableBaseTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using PG.StarWarsGame.Files.MEG.Binary.Metadata;
 using Xunit;
 
@@ -47,10 +48,12 @@ public abstract class MegFileTableBaseTest
         using var enumerator = table.GetEnumerator();
         enumerator.MoveNext();
         Assert.Equal(table[0], enumerator.Current);
+        Assert.Equal(table[0], ((IEnumerator)enumerator).Current);
 
         enumerator.Reset();
 
         enumerator.MoveNext();
         Assert.Equal(table[0], enumerator.Current);
+        Assert.Equal(table[0], ((IEnumerator)enumerator).Current);
     }
 }

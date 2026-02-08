cbc95213caa4c5570542debb4c8d2070a64bdfd4
AnakinRaW <anakin@gmx-topmail.de>
2026-02-08 11:47:33 +0100

    add encryption check to MegV1SizeCalculator and MegV2SizeCalculator

--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/Size/MegV1SizeCalculator.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/Size/MegV1SizeCalculator.cs
@@ -1,3 +1,4 @@
+using System;
 using PG.StarWarsGame.Files.MEG.Binary.Metadata.V1;
 using PG.StarWarsGame.Files.MEG.Data;
 
@@ -7,5 +8,10 @@ internal sealed class MegV1SizeCalculator : MegSizeCalculator
 {
     protected override uint HeaderSize => (uint)MegHeader.SizeValue;
 
-    protected override uint GetFileTableRecordSize(MegDataEntryBuilderInfo dataEntry) => (uint)MegFileTableRecord.SizeValue;
+    protected override uint GetFileTableRecordSize(MegDataEntryBuilderInfo dataEntry)
+    {
+        if (dataEntry.Encrypted)
+            throw new NotSupportedException("Encryption is not supported for this calculator.");
+        return (uint)MegFileTableRecord.SizeValue;
+    }
 }
--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/Size/MegV2SizeCalculator.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/Size/MegV2SizeCalculator.cs
@@ -1,4 +1,5 @@
 using PG.StarWarsGame.Files.MEG.Data;
+using System;
 
 namespace PG.StarWarsGame.Files.MEG.Binary.Size;
 
@@ -6,5 +7,10 @@ internal sealed class MegV2SizeCalculator : MegSizeCalculator
 {
     protected override uint HeaderSize => 20u;
 
-    protected override uint GetFileTableRecordSize(MegDataEntryBuilderInfo dataEntry) => 20u;
+    protected override uint GetFileTableRecordSize(MegDataEntryBuilderInfo dataEntry)
+    {
+        if (dataEntry.Encrypted)
+            throw new NotSupportedException("Encryption is not supported for this calculator.");
+        return 20u;
+    }
 }

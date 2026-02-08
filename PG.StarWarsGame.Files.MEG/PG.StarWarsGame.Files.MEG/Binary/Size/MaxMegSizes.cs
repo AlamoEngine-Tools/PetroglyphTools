b84771e9789f04d1c41c6af536450e9745caf4f9
AnakinRaW <anakin@gmx-topmail.de>
2026-02-08 17:44:58 +0100

    add MIT license header to all source files in the PG.StarWarsGame.Files.MEG project

--- PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/Size/MaxMegSizes.cs
+++ PG.StarWarsGame.Files.MEG/PG.StarWarsGame.Files.MEG/Binary/Size/MaxMegSizes.cs
@@ -1,4 +1,7 @@
-﻿namespace PG.StarWarsGame.Files.MEG.Binary.Size;
+﻿// Copyright (c) Alamo Engine Tools and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+namespace PG.StarWarsGame.Files.MEG.Binary.Size;
 
 internal readonly ref struct MaxMegSizes
 {
